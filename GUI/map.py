dbe88ee459274829851e90f5f30d85b0c84c0993
JoshuaDavis10 <jjdavis5760@gmail.com>
2025-04-23 09:05:35 -0500

    hovered regions change color + selected regions change color... its laggy tho

--- GUI/map.py
+++ GUI/map.py
@@ -7,6 +7,11 @@ import fiona
 import shapefile
 from shapely.geometry import (Point, shape)
 from datapanel import DataPanel
+import time
+
+start_time = ''
+end_time = ''
+shp = fiona.open(r"map\Select Counties\selected.shp")
 
 def LDEBUG(message):
     print(f"[DEBUG]: {message}")
@@ -23,35 +28,65 @@ class MapCanvas(FigureCanvas):
 
         self.datapanel = datapanel
 
-        fig = Figure(dpi=100)
-        self.figure = fig
-        self.canvas = FigureCanvas(self.figure)
-        self.axes=fig.add_subplot()
+        self.figure = Figure()
+        self.axes=self.figure.add_subplot()
         self.axes.set_axis_off()
         self.counties = geopandas.read_file(r"map\Select Counties\selected.shp")
         self.counties['coords'] = self.counties['geometry'].apply(lambda x: x.representative_point().coords[:])
         self.counties['coords'] = [coords[0] for coords in self.counties['coords']]
+        self.last_patch = ''
+        self.selected_patch = ''
 
         cmap = plt.cm.Pastel1
         plot = self.counties.plot(ax=self.axes, cmap=cmap)
         self.counties.apply(lambda x: plot.annotate(text=x['CNTY_NM'], xy=x.geometry.centroid.coords[0], ha='center'), axis=1)
 
         self.mpl_connect("button_press_event", self.on_click)
+        self.mpl_connect("motion_notify_event", self.on_hover)
         self.draw()
 
     def on_click(self, event):
 
         min_distance = 3
-        mouse_pos = Point(event.xdata, event.ydata)
+        if event.xdata != None: 
+            if event.ydata != None:
+                mouse_pos = Point(event.xdata, event.ydata)
+            else:
+                return
+        else:
+            return
 
-        shp = shapefile.Reader(r"map\Select Counties\selected.shp")
-        fields = shp.fields[1:]
-        field_names = [field[0] for field in fields]
+        for rec in shp:
+            boundary = shape(rec['geometry'])
+            if boundary.distance(mouse_pos) < min_distance:
+                name = rec['properties']['CNTY_NM']
+                patch = plt.Polygon(list(boundary.exterior.coords), closed=True, facecolor='green')
+                if self.selected_patch != '':
+                    self.selected_patch.remove()
+                self.axes.add_patch(patch)
+                self.selected_patch = patch
+                self.draw()
 
-        with fiona.open(r"map\Select Counties\selected.shp") as shp:
-            for rec in shp:
-                boundary = shape(rec['geometry'])
-                if boundary.distance(mouse_pos) < min_distance:
-                    name = rec['properties']['CNTY_NM']
-                    print(name)
-                    self.datapanel.on_county_selected(name)
+    def on_hover(self, event):
+
+        start_time = time.process_time()
+
+        min_distance = 3 
+        if event.xdata != None: 
+            if event.ydata != None:
+                mouse_pos = Point(event.xdata, event.ydata)
+            else:
+                return
+        else:
+            return
+
+        for rec in shp:
+            boundary = shape(rec['geometry'])
+            if boundary.distance(mouse_pos) < min_distance:
+                name = rec['properties']['CNTY_NM']
+                patch = plt.Polygon(list(boundary.exterior.coords), closed=True, facecolor='gray')
+                if self.last_patch != '':
+                    self.last_patch.remove()
+                self.axes.add_patch(patch)
+                self.last_patch = patch
+                self.draw()
